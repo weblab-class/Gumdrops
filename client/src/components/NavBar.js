1e372f63f89051b1d2857cf10ae4a46dfab3234e
grayhimakar2 <huydai.gsmst@gmail.com>
2021-01-13 20:11:29 -0500

    Populate NavBar.js with initial links

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
-
+import {Link} from "@reach/router";
 class NavBar extends Component {
     constructor(props) {
         super(props);
       }
     render() {
-        return <div></div>;
+        return(
+        <nav className="NavBar-container">
+            <div className="NavBar-title u-inlineBlock">Gumdrops</div>
+            <div className="NavBar-linkContainer u-inlineBlock">
+                <Link to="/">Home</Link>
+                <Link to="/explore">Explore</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/profile">Profile</Link>
+            </div>
+        </nav>);
     }
 }
 export default NavBar;
