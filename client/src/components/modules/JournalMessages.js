ec754965a3e26fb0512cdedeacc1fcfd1781b82b
grayhimakar2 <huydai.gsmst@gmail.com>
2021-01-16 16:04:03 -0500

    Adddate header between messages of different days

--- client/src/components/modules/JournalMessages.js
+++ client/src/components/modules/JournalMessages.js
@@ -31,15 +31,34 @@ class JournalMessages extends Component {
   constructor(props) {
     super(props);
   }
-
   render() {
+    let renderOutput = [];
+    let lastTimeStamp;
+    if(this.props.data.messages[0]){ 
+      lastTimeStamp=this.props.data.messages[0].timestamp.substr(0,10);
+      renderOutput.push(<h2>{lastTimeStamp}</h2>); //header for first date
+    }
+    for(var i=0; i<this.props.data.messages.length;i++){
+      let currTimeStamp = this.props.data.messages[i].timestamp.substr(0,10);
+      if (currTimeStamp!==lastTimeStamp){
+        console.log("Date was pushed here");
+        renderOutput.push(<h2>{currTimeStamp}</h2>);
+        lastTimeStamp = currTimeStamp;
+      }
+      renderOutput.push(<JournalMessage message={this.props.data.messages[i]} key={i}/>);
+    }
+    console.log(renderOutput);
+    console.log("Type of: "+typeof(renderOutput));
     return (
       <div className="u-flexColumn JournalMessages-container">
         <h3 className="u-textCenter">Team Journal</h3>
-        <div className="JournalMessages-historyContainer">
+        {/*<div className="JournalMessages-historyContainer">
           {this.props.data.messages.map((m, i) => (
             <JournalMessage message={m} key={i} />
           ))}
+          </div>*/}
+        <div className="JournalMessages-historyContainer">
+          {renderOutput}
         </div>
         <div className="JournalMessages-newContainer">
           <NewJournalMessage recipient={this.props.data.recipient} />
