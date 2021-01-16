dc63e078586d8457b7aa364979f205d431436c61
grayhimakar2 <huydai.gsmst@gmail.com>
2021-01-16 16:24:36 -0500

    Add hour:minute timestamp to JournalMessages

--- client/src/components/modules/JournalMessage.js
+++ client/src/components/modules/JournalMessage.js
@@ -15,15 +15,42 @@ class JournalMessage extends Component {
 
 
   render() {
+    //Date stamps longer being used - Harry
+    /*
     const dd = this.props.message.timestamp.substring(8, 10);
     const mm = this.props.message.timestamp.substring(5, 7);
     const yyyy = this.props.message.timestamp.substring(0, 4);
     const timeStamp = mm + "/" + dd + "/" + yyyy;
+    */
+    const hh = parseInt(this.props.message.timestamp.substring(11, 13));
+    const mm = this.props.message.timestamp.substring(14, 16);
+    let isAM = true;
+
+    let readableTime = "";
+    if(hh<12){
+      if(hh===0){
+        readableTime = "12:";
+      } else {
+        readableTime = ""+hh+":";
+      }
+    } else {
+      readableTime = ""+(hh-12)+":";
+      isAM = false;
+    }
+    readableTime += mm;
+    if(isAM){
+      readableTime += " AM";
+    } else {
+      readableTime += " PM";
+    }
+    console.log("Entire timestamp "+this.props.message.timestamp);
+    console.log("hmStamp "+hh +mm);
 
     return (
       <div className={"u-flex u-flex-alignCenter JournalMessage-container"}>
-        <span className=" JournalMessage-sender u-bold">{this.props.message.sender.name + ":"}</span>
-        <span className="JournalMessage-content">{"(" + timeStamp + ")  " + this.props.message.content}</span>
+        <span className="JournalMessage-sender u-bold">{this.props.message.sender.name}</span>
+        <span className="JournalMessage-time">{readableTime}</span>
+        <span className="JournalMessage-content">{this.props.message.content}</span>
       </div>
     );
   }
