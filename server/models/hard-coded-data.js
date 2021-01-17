dde6c5e477362e75411ea6382cf5885aed285fcd
grayhimakar2 <huydai.gsmst@gmail.com>
2021-01-16 23:05:54 -0500

    Fix GET and POST api route for /storycards

--- server/models/hard-coded-data.js
+++ server/models/hard-coded-data.js
@@ -16,3 +16,18 @@ let project1 = new Project({
       }],
   });
   project1.save();
+
+
+//Usage: Test the functionality of GET and POST at /api/storycards
+//location App.js
+//in handleLogin() 
+const newStoryCard = {
+  projectId: "6002597bb45b7733b322e9ad",
+  textTitle: "This is the start of my project!",
+  textContent: "Welcome. My name is Miami and this is my amazing adventure...",
+  links: ["https://www.google.com","https://www.youtube.com"],
+  videoMedia: "https://www.youtube.com/watch?v=Ff0wYvW2JKs"
+};
+post("/api/storycards",newStoryCard);
+//in handleLogout()
+get("/api/storycards",{projectId:"6002597bb45b7733b322e9ad"}).then((storycards)=>console.log(storycards));
