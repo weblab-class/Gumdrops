f74f2fcc5f5b69435c89cff761f689e78ca4e98f
DanielV <danielvi@mit.edu>
2021-01-18 19:36:14 -0600

    Added roles to dashboard projects

--- client/src/components/modules/ProjectDisplay.js
+++ client/src/components/modules/ProjectDisplay.js
@@ -7,18 +7,53 @@ import Thumbnail from "./Thumbnail.js";
 
 //Props
 //userId : String (passed down from App.js)
-//projectId : String (passed down from Projects.js)
-//projectName : String (passed down from Projects.js)
+//projectId : String (passed down from Projects.js/Explore.js)
+//projectName : String (passed down from Projects.js/Explore.js)
+//showRole : Boolean
 
 class ProjectDisplay extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            role : null,
+        }
     }
+
     componentDidMount(){
+        if (this.props.showRole) {
+            get("/api/project", {projectId:this.props.projectId}).then((projectObj)=>{
+                console.log("Project found: "+ projectObj.name)
+                let users = projectObj.collaborators;
+                users.forEach((user)=> {
+                    console.log("Currently looking at user: "+user.userId);
+                    if(user.userId === this.props.userId) {
+                        console.log("Collaborator found with role: "+user.role);
+                        this.setState({
+                            role: user.role,
+                        })
+                    };
+                });
+            }).then(console.log("User Role: "+this.state.role))
+        };
     }
 
     render() {
         console.log("ProjectDisplay was called.");
+        if (this.props.showRole) {
+            return (
+                <div className="ProjectDisplay-container">
+                    <Link to={`/project/${this.props.projectId}`}>
+                        <div className="ProjectDisplay-image">
+                            <Thumbnail projectId={this.props.projectId} />
+                        </div>
+                        <div className="ProjectDisplay-text u-inlineBlock u-textCenter">
+                            <div>{this.props.projectName}</div>
+                            <div>{"@"+this.state.role}</div>
+                        </div>
+                    </Link>
+                </div>
+            );
+        };
         return (
             <div className="ProjectDisplay-container">
                 <Link to={`/project/${this.props.projectId}`}>
