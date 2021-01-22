19790874ae75e6d2fda1dbea23802e5cbdf05788
grayhimakar2 <huydai.gsmst@gmail.com>
2021-01-22 16:11:04 -0500

    Force long line of text to wrap, add Cursor component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -49,6 +49,8 @@ class App extends Component {
 
   };
 
+  
+  
   render() {
     return (
       <>
---
+++ client/src/components/modules/Cursor.js
@@ -0,0 +1,21 @@
+import React, {Component} from "react";
+import "./NewPostInput.css";
+import {post} from "../../utilities";
+
+/**
+ * Cursor is a component used to provide unique styling to the cursor based on specific actions.
+ * 
+ * Proptypes 
+ * 
+ */
+
+ class DeleteProject extends Component{
+     constructor(props){
+         super(props);
+     }
+    render(){
+        return(<></>
+        );
+    }
+ }
+ export default DeleteProject;
