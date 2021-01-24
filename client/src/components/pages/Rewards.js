f11172285642b8f868c92fb4867cb276cbf3d46d
DanielV <danielvi@mit.edu>
2021-01-23 18:44:08 -0600

    Created a hard coded skeleton for Rewards page

--- client/src/components/pages/Rewards.js
+++ client/src/components/pages/Rewards.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-
+import "./Rewards.css"
 import "../../utilities.css";
+import Img from "../../public/ximage.png";
 
 class Rewards extends Component {
   constructor(props) {
@@ -14,11 +15,29 @@ class Rewards extends Component {
   }
 
   render() {
-    return (
+    return ( //hard coded for now
       <>
-      <div className="u-flexColumn u-textCenter">
-        <h1>Rewards</h1>
-      </div>
+        <div className="Rewards-achievementsContainer">
+          <h1>Achievements</h1>
+          <div className="Rewards-achievement">
+            <img className="Rewards-center" src={Img}/>
+            <h4 className="u-textCenter">Web Lab Master</h4>
+          </div>
+        </div>
+        <div className="Rewards-proUnlockContainer u-inlineBlock">
+          <h1>Progress</h1>
+          <div className="Rewards-progress">
+            <img className="Rewards-center" src={Img}/>
+            <h4 className="u-textCenter">20/100 Page Views</h4>
+          </div>
+        </div>
+        <div className="Rewards-proUnlockContainer u-inlineBlock">
+          <h1>Unlockables</h1>
+          <div className="Rewards-unlockables">
+            <img className="Rewards-center" src={Img}/>
+            <h4 className="u-textCenter">Space Banner</h4>
+          </div>
+        </div>
       </>
     );
   }
