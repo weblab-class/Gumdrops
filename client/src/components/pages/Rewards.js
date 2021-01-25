fc96476f288d56faac9dc493e3dbfde107566436
DanielV <danielvi@mit.edu>
2021-01-24 18:00:59 -0600

    Added more hardcoded stuff to the rewards page

--- client/src/components/pages/Rewards.js
+++ client/src/components/pages/Rewards.js
@@ -7,7 +7,42 @@ class Rewards extends Component {
   constructor(props) {
     super(props);
     // Initialize Default State
-    this.state = {};
+    this.state = {
+      achievements: [
+        {
+          imageSource: Img,
+          title: "Baby Gummy",
+          progress: "Complete"
+        },
+        {
+          imageSource: Img,
+          title: "Social Butterfly",
+          progress: "3/5 Journal Entries"
+        },
+        {
+          imageSource: Img,
+          title: "Web Lab Master",
+          progress: "20/100 Page Views"
+        },
+      ],
+      unlockables: [
+        {
+          imageSource: Img,
+          title: "Space Banner",
+          progress: "5/10 Projects"
+        },
+        {
+          imageSource: Img,
+          title: "Color Theme",
+          progress: "0/1 Story Cards"
+        },
+        {
+          imageSource: Img,
+          title: "Role @Dragonator",
+          progress: "4/10 People Tagged"
+        },
+      ]
+    };
   }
 
   componentDidMount() {
@@ -15,23 +50,27 @@ class Rewards extends Component {
   }
 
   render() {
-    return ( //hard coded for now
+    return (
       <>
-        <div className="Rewards-achievementsContainer">
-          <h1>Achievements</h1>
-          <div className="Rewards-achievement">
-            <img className="Rewards-centerImg" src={Img}/>
-            <h3 className="u-textCenter">Web Lab Master</h3>
-            <h4 className="u-textCenter">20/100 Page Views</h4>
-          </div>
+        <div /*className="Rewards-achievementsContainer"*/>
+          <h1 className="Rewards-title">Achievements</h1>
+            {this.state.achievements.map((reward)=>(
+              <div className="Rewards-achievement u-inlineBlock">
+                <img className="Rewards-centerImg" src={reward.imageSource}/>
+                <h3 className="u-textCenter">{reward.title}</h3>
+                <h4 className="u-textCenter">{reward.progress}</h4>
+              </div>
+            ))}
         </div>
-        <div className="Rewards-unlockContainer u-inlineBlock">
-          <h1>Unlockables</h1>
-          <div className="Rewards-unlockables">
-            <img className="Rewards-centerImg" src={Img}/>
-            <h3 className="u-textCenter">Space Banner</h3>
-            <h4 className="u-textCenter">5/10 Projects</h4>
-          </div>
+        <div /*className="Rewards-unlockContainer u-inlineBlock"*/>
+          <h1 className="Rewards-title">Unlockables</h1>
+            {this.state.unlockables.map((reward)=>(
+              <div className="Rewards-unlockables u-inlineBlock">
+                <img className="Rewards-centerImg" src={reward.imageSource}/>
+                <h3 className="u-textCenter">{reward.title}</h3>
+                <h4 className="u-textCenter">{reward.progress}</h4>
+              </div>
+            ))}
         </div>
       </>
     );
