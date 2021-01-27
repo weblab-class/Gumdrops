cf4ef0d3161b5611b9239b8b268671232b8ae93e
grayhimakar2 <huydai.gsmst@gmail.com>
2021-01-27 11:31:03 -0500

    Fix up GetStarted page

--- client/src/components/pages/GetStarted.js
+++ client/src/components/pages/GetStarted.js
@@ -43,13 +43,15 @@ import gumdrops_icon from "../../public/GumdropsIcon_Transparent.png";
             <>
                 <h2 className="u-textCenter Profile-welcome">Welcome, {!this.state.user ? "Anonymous" : this.state.user.name}</h2>
                 <hr></hr>
-               <h1 className="u-textCenter getStarted-title">Get Started with using<img style={{"verticalAlign":"middle"}} src={gumdrops_icon}></img></h1>
+               <h1 className="u-textCenter getStarted-title">Get Started with using<img style={{"verticalAlign":"-3vh","height":"9vh"}} src={gumdrops_icon}></img></h1>
                <div className="getStarted-instruct-container">
                    <div className="getStarted-step-container">
                        <section className="getStarted-leftStep-container">
-                           <h2 className="getStarted-textTitle">1. Create your first project</h2>
-                           <h4>Head to the "My Projects" tab in your navbar and click on the gray button with a plus sign to create your first project!</h4>
-                           <h4>Make sure to fill out your project name, add any collaborators that you are working with (don't include yourself), and assign all your group members a role.</h4>
+                           <article className="getStarted-leftStep-text">
+                            <h2 className="getStarted-textTitle">1. Create your first project</h2>
+                            <p className="getStarted-subText">Head to the "My Projects" tab in your navbar and click on the gray button with a plus sign to create your first project!</p>
+                            <p className="getStarted-subText">Make sure to fill out your project name, add any collaborators that you are working with (don't include yourself), and assign all your group members a role.</p>
+                           </article>
                        </section>
                        <section className="getStarted-rightStep-container">
                            <p>This should be where an image resisdes</p>
@@ -59,10 +61,11 @@ import gumdrops_icon from "../../public/GumdropsIcon_Transparent.png";
                <div className="getStarted-instruct-container">
                    <div className="getStarted-step-container">
                        <section className="getStarted-leftStep-container">
-                           <h2 className="getStarted-textTitle">2. Populate your project with content</h2>
-                           <h4>Your project consists of two main components, the story cards and journal. Story cards are where you can save your images, project descriptions, and links to your project folder and documents.</h4>
-                           <h4>The journal is where you and your teammates can keep logs of updates and progress. Think of it as a daily diary where you can look back on what happened on a particular day (Note: The message timestamps are in EST)</h4>
-
+                           <article className="getStarted-leftStep-text">
+                               <h2 className="getStarted-textTitle">2. Populate your project with content</h2>
+                               <p className="getStarted-subText">Your project consists of two main components, the story cards and journal. Story cards are where you can save your images, project descriptions, and links to your project folder and documents.</p>
+                               <p className="getStarted-subText">The journal is where you and your teammates can keep logs of updates and progress. Think of it as a daily diary where you can look back on what happened on a particular day (Note: The message timestamps are in EST)</p>
+                           </article>
                        </section>
                        <section className="getStarted-rightStep-container">
                            <p>This should be where an image resisdes</p>
@@ -72,9 +75,10 @@ import gumdrops_icon from "../../public/GumdropsIcon_Transparent.png";
                <div className="getStarted-instruct-container">
                    <div className="getStarted-step-container">
                        <section className="getStarted-leftStep-container">
-                           <h2 className="getStarted-textTitle">3. Earn rewards</h2>
-                           <h4>As you work on your projects, you can earn rewards for having story cards, journal tags, and more! Tagging is where you mention your team member names in the journal (e.g, "@Huy_Dai worked on the kalimba today"). You can even tag yourself!</h4>
-        
+                            <article className="getStarted-leftStep-text">
+                                <h2 className="getStarted-textTitle">3. Earn rewards</h2>
+                                <p className="getStarted-subText">As you work on your projects, you can earn rewards for having story cards, journal tags, and more! Tagging is where you mention your team member names in the journal (e.g, "@Huy_Dai worked on the kalimba today"). You can even tag yourself!</p>
+                            </article>
                        </section>
                        <section className="getStarted-rightStep-container">
                            <p>This should be where an image resisdes</p>
@@ -84,9 +88,11 @@ import gumdrops_icon from "../../public/GumdropsIcon_Transparent.png";
                <div className="getStarted-instruct-container">
                    <div className="getStarted-step-container">
                        <section className="getStarted-leftStep-container">
-                           <h2 className="getStarted-textTitle">4. Explore other Gumdrops projects!</h2>
-                           <h4>Gumdrops was created with the intention of creating a community of creative-learners. The Explore page is a great way to take a look at projects created by other members of the community.</h4>
-                           <h4>Currently, all projects made on the website are publicly viewable. In the future, we plan to add an option for users to choose whether to show or hide their project in the Explore page</h4>
+                            <article className="getStarted-leftStep-text">
+                                <h2 className="getStarted-textTitle">4. Explore other Gumdrops projects!</h2>
+                                <p className="getStarted-subText">Gumdrops was created with the intention of creating a community of creative-learners. The Explore page is a great way to take a look at projects created by other members of the community.</p>
+                                <p className="getStarted-subText">Currently, all projects made on the website are publicly viewable. In the future, we plan to add an option for users to choose whether to show or hide their project in the Explore page</p>
+                            </article>
                       </section>
                        <section className="getStarted-rightStep-container">
                            <p>This should be where an image resisdes</p>
