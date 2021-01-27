f5dd57934a5c1fee5c1fdc602f3d628af925d435
grayhimakar2 <huydai.gsmst@gmail.com>
2021-01-27 01:56:06 -0500

    Add skeleton to GetStarted page

--- client/src/components/pages/GetStarted.js
+++ client/src/components/pages/GetStarted.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import "./Profile.css";
 import "../../utilities.css";
+import "./GetStarted.css";
+import gumdrops_icon from "../../public/GumdropsIcon_Transparent.png";
 
 /**
  * Get Started Page it tells everyone how to use the Gumdrops App
@@ -18,9 +20,29 @@ import "../../utilities.css";
 
      render(){
          return(
-             
-             <h1 className="u-textCenter">Get Started with using Gumdrops</h1>
-
+            <>
+               <h1 className="u-textCenter getStarted-title">Get Started with using<img style={{"verticalAlign":"middle"}} src={gumdrops_icon}></img></h1>
+               <div className="getStarted-instruct-container">
+                   <div className="getStarted-step-container">
+                       <section className="getStarted-leftStep-container">
+                           <h2>1. This is the instruction for the step</h2>
+                       </section>
+                       <section className="getStarted-rightStep-container">
+                           <p>This should be where an image resisdes</p>
+                       </section>
+                   </div>
+               </div>
+               <div className="getStarted-instruct-container">
+                   <div className="getStarted-step-container">
+                       <section className="getStarted-leftStep-container">
+                           <h2>1. This is the instruction for the step</h2>
+                       </section>
+                       <section className="getStarted-rightStep-container">
+                           <p>This should be where an image resisdes</p>
+                       </section>
+                   </div>
+               </div>
+            </>
          )
      }
  }
