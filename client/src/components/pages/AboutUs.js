642b82707a2609801395d0f0b838d641ea04083b
El-Guapo2024 <68251336+El-Guapo2024@users.noreply.github.com>
2021-02-03 15:49:05 -0600

    added link

--- client/src/components/pages/AboutUs.js
+++ client/src/components/pages/AboutUs.js
@@ -22,6 +22,10 @@ import "./AboutUs.css";
                     eventually sponsored as an ELO (Experiential Learning Opportunity) by the MIT Edgerton Center under Ed Moriarity. The initial plan for Gumdrops was to develop a crowdsourcing platform for students, by students, to share games, simulations, and videos about educational topics that they are interested in. 
                 </h4>
                 <h4 className="AboutUs-text">Over time, as the group grew and expanded, Gumdrops saw multiple revisions of its focus, shifting to a virtual classroom/ social media hybrid where teachers can post verified educational content and students could share images and videos of their project. Finally, we settled on creating a platform for teams to document and share their project experience through the method of storytelling that highlights the human aspect of every project. This website represents the first prototype of Gumdrops. In the future, we hope to continue to evolve and develop this website into one that would allow for more people to connect and collaborate in building a bigger, more tight-knit Gumdrops community.</h4>
+                <h2 className = "AboutUs-title">Learn More About Gumdrops and Our Story</h2>
+                <a href = "https://docs.google.com/document/d/1R2ax4_L_r418z5BaYQHBzgut9LhmcgW-SV0zG-kRi8I/edit?usp=sharing" target = "_blank">
+                    <h4 className = "AboutUs-text">Gumdrops Through the Ages </h4>
+                </a>
             </>
          )
      }
